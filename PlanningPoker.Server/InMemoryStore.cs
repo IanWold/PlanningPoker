efb2afce4f2547c92f6a75719f37e62dd3000d9d
Ian Wold <1645320+IanWold@users.noreply.github.com>
2024-04-11 18:10:29 +0000

    Make in-memory store members public

--- PlanningPoker.Server/InMemoryStore.cs
+++ PlanningPoker.Server/InMemoryStore.cs
@@ -2,47 +2,47 @@ namespace PlanningPoker.Server;
 
 public class InMemoryStore : IStore
 {
-    Task CreateParticipantAsync(Guid sessionId, string participantId, string name)
+    public Task CreateParticipantAsync(Guid sessionId, string participantId, string name)
     {
         throw new NotImplementedException();
     }
 
-    Task<Guid> CreateSessionAsync(string title)
+    public Task<Guid> CreateSessionAsync(string title)
     {
         throw new NotImplementedException();
     }
 
-    Task DeleteParticipantAsync(Guid sessionId, string participantId)
+    public Task DeleteParticipantAsync(Guid sessionId, string participantId)
     {
         throw new NotImplementedException();
     }
 
-    Task<bool> ExistsSessionAsync(Guid sessionId)
+    public Task<bool> ExistsSessionAsync(Guid sessionId)
     {
         throw new NotImplementedException();
     }
 
-    Task<Session?> GetSessionAsync(Guid sessionId)
+    public Task<Session?> GetSessionAsync(Guid sessionId)
     {
         throw new NotImplementedException();
     }
 
-    Task UpdateParticipantNameAsync(Guid sessionId, string name)
+    public Task UpdateParticipantNameAsync(Guid sessionId, string name)
     {
         throw new NotImplementedException();
     }
 
-    Task UpdateParticipantPointsAsync(Guid sessionId, string points)
+    public Task UpdateParticipantPointsAsync(Guid sessionId, string points)
     {
         throw new NotImplementedException();
     }
 
-    Task UpdateSessionStateAsync(Guid sessionId, State state)
+    public Task UpdateSessionStateAsync(Guid sessionId, State state)
     {
         throw new NotImplementedException();
     }
 
-    Task UpdateSessionTitleAsync(Guid sessionId, string title)
+    public Task UpdateSessionTitleAsync(Guid sessionId, string title)
     {
         throw new NotImplementedException();
     }

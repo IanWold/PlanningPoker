3a20edf9c8420a415fbd78ae62e98d172c439985
IanWold <ilwold@hotmail.com>
2025-08-10 08:17:49 -0500

    Log method results

--- PlanningPoker.Server/HubLoggingFilter.cs
+++ PlanningPoker.Server/HubLoggingFilter.cs
@@ -5,12 +5,20 @@ namespace PlanningPoker.Server;
 
 public class HubLoggingFilter(ILogger<HubLoggingFilter> logger) : IHubFilter {
     public async ValueTask<object?> InvokeMethodAsync(HubInvocationContext invocationContext, Func<HubInvocationContext, ValueTask<object?>> next) {
-        _ = Task.Run(()=> logger.LogInformation("Client {ConnectionId} called {MethodName} with args: {@Args}",
+        _ = Task.Run(() => logger.LogInformation("Client {ConnectionId} called {MethodName} with args: {Args}",
             invocationContext.Context.ConnectionId,
             invocationContext.HubMethodName,
             string.Join(", ", invocationContext.HubMethodArguments.Select(o => JsonSerializer.Serialize(o)))
         ));
 
-        return await next(invocationContext);
+        var result = await next(invocationContext);
+
+        _ = Task.Run(() => logger.LogInformation("Call to {MethodName} from client {ConnectionId} responding with: {Result}",
+            invocationContext.Context.ConnectionId,
+            invocationContext.HubMethodName,
+            JsonSerializer.Serialize(result)
+        ));
+
+        return result;
     }
 }

8631f428d216c5793d284d842ed86cd5f5bb2efe
IanWold <ilwold@hotmail.com>
2025-11-14 13:46:25 -0600

    Shorten in memory store

--- PlanningPoker.Server/InMemoryStore.cs
+++ PlanningPoker.Server/InMemoryStore.cs
@@ -4,8 +4,7 @@ public class InMemoryStore : IStore {
     static readonly Dictionary<string, Session> _sessions = [];
 
     private static Task UpdateSession(string sessionId, Func<Session, Session> update) {
-        var session = _sessions[sessionId];
-        _sessions[sessionId] = update(session);
+        _sessions[sessionId] = update(_sessions[sessionId]);
         return Task.CompletedTask;
     }
 
@@ -15,14 +14,10 @@ public class InMemoryStore : IStore {
     }
 
     public Task CreateParticipantAsync(string sessionId, string participantId, string name) =>
-        UpdateSession(sessionId, session => session with {
-            Participants = [.. session.Participants, new(participantId, name, "", 0) ]
-        });
+        UpdateSession(sessionId, session => session with { Participants = [.. session.Participants, new(participantId, name, "", 0) ] });
 
     public Task AddPointAsync(string sessionId, string point) =>
-        UpdateSession(sessionId, session => session with {
-            Points = [.. session.Points, point]
-        });
+        UpdateSession(sessionId, session => session with { Points = [.. session.Points, point] });
 
     public Task<string> CreateSessionAsync(string title, IEnumerable<string> points) {
         string newSessionId;
@@ -38,9 +33,7 @@ public class InMemoryStore : IStore {
     }
 
     public Task DeleteParticipantAsync(string sessionId, string participantId) =>
-        UpdateSession(sessionId, session => session with {
-            Participants = session.Participants.Where(p => p.ParticipantId != participantId).ToArray()
-        });
+        UpdateSession(sessionId, session => session with { Participants = session.Participants.Where(p => p.ParticipantId != participantId).ToArray() });
 
     public Task<bool> ExistsSessionAsync(string sessionId) =>
         Task.FromResult(_sessions.ContainsKey(sessionId));
@@ -49,37 +42,23 @@ public class InMemoryStore : IStore {
         Task.FromResult(_sessions.TryGetValue(sessionId, out var session) ? session : null);
 
     public Task IncrementParticipantStarsAsync(string sessionId, string participantId, int count = 1) =>
-        UpdateParticipant(sessionId, participantId, participant => participant with {
-            Stars = participant.Stars + count
-        });
+        UpdateParticipant(sessionId, participantId, participant => participant with { Stars = participant.Stars + count });
 
     public Task RemovePointAsync(string sessionId, string point) =>
-        UpdateSession(sessionId, session => session with {
-            Points = [.. session.Points.Except([point])]
-        });
+        UpdateSession(sessionId, session => session with { Points = [.. session.Points.Except([point])] });
 
     public Task UpdateAllParticipantPointsAsync(string sessionId, string points = "") =>
-        UpdateSession(sessionId, session => session with {
-            Participants = [.. session.Participants.Select(p => p with { Points = points})]
-        });
+        UpdateSession(sessionId, session => session with { Participants = [.. session.Participants.Select(p => p with { Points = points})] });
 
     public Task UpdateParticipantNameAsync(string sessionId, string participantId, string name) =>
-        UpdateParticipant(sessionId, participantId, participant => participant with {
-            Name = name
-        });
+        UpdateParticipant(sessionId, participantId, participant => participant with { Name = name });
 
     public Task UpdateParticipantPointsAsync(string sessionId, string participantId, string points) =>
-        UpdateParticipant(sessionId, participantId, participant => participant with {
-            Points = points
-        });
+        UpdateParticipant(sessionId, participantId, participant => participant with { Points = points });
 
     public Task UpdateSessionStateAsync(string sessionId, State state) =>
-        UpdateSession(sessionId, session => session with {
-            State = state
-        });
+        UpdateSession(sessionId, session => session with { State = state });
 
     public Task UpdateSessionTitleAsync(string sessionId, string title) =>
-        UpdateSession(sessionId, session => session with {
-            Title = title
-        });
+        UpdateSession(sessionId, session => session with { Title = title });
 }
